eab42556114df9f834e96c2745b8a14c33f586e7
Cherisle <michaeltran95@live.com>
2017-01-09 21:08:14 -0800

    Beginning Transfer

---
+++ Assets/GameBoundary.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBoundary : MonoBehaviour
+{
+	public int rows;
+	public int columns;
+	public GameObject[,] array2D;
+	public GameObject myObject;
+	private float maxRayDistX, maxRayDistY;
+	private float ctrXLoc, ctrYLoc;
+	private Vector2 rectNWCorner,rectNECorner,rectSWCorner;
+	private Ray2D northSide,eastSide,southSide,westSide;
+	private RaycastHit2D[] hitNorth,hitEast,hitSouth,hitWest;
+	private RaycastHit2D hitN,hitE,hitS,hitW;
+
+	void FixedUpdate()
+	{
+		Debug.DrawLine (rectNWCorner, rectNWCorner + Vector2.right * maxRayDistX);
+		Debug.DrawLine (rectNECorner, rectNECorner + Vector2.down * maxRayDistY);
+		Debug.DrawLine (rectSWCorner, rectSWCorner + Vector2.right * maxRayDistX);
+		Debug.DrawLine (rectNWCorner, rectNWCorner + Vector2.down * maxRayDistY);
+	}
+
+	void Start ()
+	{
+		maxRayDistX = GetComponent<RectTransform>().sizeDelta.x; // stretches the width
+		maxRayDistY = GetComponent<RectTransform>().sizeDelta.y; // stretches the height
+		ctrXLoc = transform.position.x;
+		ctrYLoc = transform.position.y;
+		rectNWCorner = new Vector2 (ctrXLoc - maxRayDistX / 2, ctrYLoc + maxRayDistY / 2);
+		rectNECorner = new Vector2 (ctrXLoc + maxRayDistX / 2, ctrYLoc + maxRayDistY / 2);
+		rectSWCorner = new Vector2 (ctrXLoc - maxRayDistX / 2, ctrYLoc - maxRayDistY / 2);
+		northSide = new Ray2D (rectNWCorner, Vector2.right);
+		eastSide = new Ray2D (rectNECorner, Vector2.down);
+		southSide = new Ray2D (rectSWCorner, Vector2.right);
+		westSide = new Ray2D (rectNWCorner, Vector2.down);
+		//--------------------------------------------------------------------------------
+		myObject = Resources.Load("Default/DefaultTile") as GameObject;
+		array2D = new GameObject[columns,rows];
+		for(int ii=0;ii<columns;ii++)
+		{
+			for(int jj=0;jj<rows;jj++)
+			{
+				array2D [ii, jj] = Instantiate (myObject, new Vector2(ii*2-9,jj*-2+9), Quaternion.identity) as GameObject;
+				Debug.Log ("[" + ii + "," + jj + "] contains " + myObject.name);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		/*hitNorth = Physics2D.RaycastAll (northSide.origin, northSide.direction,maxRayDistX);
+		hitEast = Physics2D.RaycastAll (eastSide.origin, eastSide.direction,maxRayDistY);
+		hitSouth = Physics2D.RaycastAll (southSide.origin, southSide.direction,maxRayDistX);
+		hitWest = Physics2D.RaycastAll (westSide.origin, westSide.direction,maxRayDistY);
+		for (int ii = 0; ii < hitEast.Length; ii++)
+		{
+			RaycastHit2D hitSingleEast = hitEast [ii];
+			if (hitSingleEast != false && hitSingleEast.collider != null)
+			{
+				Debug.Log ("Detected collision with East side boundary");
+			}
+		}
+		for (int ii = 0; ii < hitSouth.Length; ii++)
+		{
+			RaycastHit2D hitSingleSouth = hitSouth [ii];
+			if (hitSingleSouth != false && hitSingleSouth.collider != null && hitSingleSouth.collider.GetComponent<AnimuHead>() != null
+				&& hitSingleSouth.collider.name != "DefaultTile")
+			{
+				Debug.Log ("Detected collision with South side boundary");
+			}
+		}
+		for (int ii = 0; ii < hitWest.Length; ii++)
+		{
+			RaycastHit2D hitSingleWest = hitWest [ii];
+			if (hitSingleWest != false && hitSingleWest.collider != null)
+			{
+				Debug.Log ("Detected collision with West side boundary");
+			}
+		}*/
+	}
+}
---
+++ Assets/TileGenerator.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class TileGenerator : MonoBehaviour
+{
+	public Object[] myPrefabs;
+	public GameObject[,] array;
+	private GameObject genLocation;
+	private int colControl;
+	private int colDestroy;
+	private GameObject go;
+	private int fallCounter;
+	// Use this for initialization
+	void Start ()
+	{
+		fallCounter = 0;
+		colDestroy = 0;
+		colControl = 5; // default row value for generator
+		genLocation = transform.parent.GetComponent<GameBoundary> ().array2D [colControl, 0];
+		myPrefabs = Resources.LoadAll ("Characters", typeof(GameObject)).Cast<GameObject> ().ToArray ();
+		CreatePrefab();
+	}
+	void CreatePrefab()
+	{
+		go = (GameObject) myPrefabs[RandomNumber()];
+		colControl = 5;
+		genLocation = Instantiate (go, transform.parent.GetComponent<GameBoundary> ().array2D [colControl,0].transform.position, Quaternion.identity) as GameObject;
+		Debug.Log ("[5,0] " + "contains " + genLocation.name);
+		InvokeRepeating ("Falling", 0.6f, 0.6f);
+	}
+	void Falling()
+	{
+		if (fallCounter < 9)
+		{	
+			if (transform.parent.GetComponent<GameBoundary> ().array2D [colControl, fallCounter + 1].GetComponent<AnimuHead>() != null)
+			{
+				CancelInvoke ("Falling");
+				if (fallCounter + 1 == 1)
+				{
+					//don't create new prefab
+				}
+				else
+				{
+					fallCounter = 0;
+					CreatePrefab ();
+				}
+			}
+			else
+			{
+				if (fallCounter == 0)
+				{
+					transform.parent.GetComponent<GameBoundary> ().array2D [colControl, fallCounter + 1] = genLocation;
+				}
+				else
+				{
+					for (int ii = 0; ii < transform.parent.GetComponent<GameBoundary> ().columns; ii++)
+					{
+						if (transform.parent.GetComponent<GameBoundary> ().array2D [ii, fallCounter].GetComponent<AnimuHead> () != null)
+						{
+							transform.parent.GetComponent<GameBoundary> ().array2D [colControl, fallCounter + 1] = transform.parent.GetComponent<GameBoundary> ().array2D [ii, fallCounter];
+							colDestroy = ii;
+							break;
+						}
+					}
+				}
+				//--------------------------------------------------------------------------------------------------------------------
+				Destroy (transform.parent.GetComponent<GameBoundary> ().array2D [colControl, fallCounter + 1]); // destroy tile below
+				transform.parent.GetComponent<GameBoundary>().array2D[colControl, fallCounter + 1] = Instantiate (transform.parent.GetComponent<GameBoundary>().array2D[colControl,fallCounter+1], new Vector2(colControl*2-9,(fallCounter+1)*-2+9), Quaternion.identity) as GameObject;
+				Destroy (transform.parent.GetComponent<GameBoundary> ().array2D [colDestroy, fallCounter]); // destroy AnimuHead tile above
+				transform.parent.GetComponent<GameBoundary>().array2D[colDestroy, fallCounter] = Instantiate (Resources.Load("Default/DefaultTile"), new Vector2(colControl*2-9,fallCounter*-2+9), Quaternion.identity) as GameObject;
+				Debug.Log ("[" + colControl + "," + (fallCounter + 1) + "] contains " + transform.parent.GetComponent<GameBoundary>().array2D[colControl,fallCounter+1]);
+				Debug.Log ("[" + colControl + "," + fallCounter + "] contains " + transform.parent.GetComponent<GameBoundary>().array2D[colControl,fallCounter].name);
+				if (fallCounter == 0)
+				{
+					Destroy (genLocation);
+				}
+				fallCounter++;
+			}
+		} 
+		else
+		{
+			CancelInvoke ("Falling");
+			fallCounter = 0;
+			CreatePrefab();
+		}
+
+	}
+	int RandomNumber()
+	{
+		System.Random rand = new System.Random();
+		return rand.Next(0,myPrefabs.Length);
+	}
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && colControl > 0)
+		{
+			colControl = colControl - 1;
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow) && colControl < 9)
+		{
+			colControl = colControl + 1;
+		}
+	}
+				
+}
