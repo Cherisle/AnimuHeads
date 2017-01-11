73bbc32e734be19ef931d94a7abf98dce37bb09c
Holly Dinh <dinh.holly@gmail.com>
2017-01-11 14:29:52 -0800

    New Character Set Added
    FateSN UBW CharPack

---
+++ Assets/Illyasviel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Illyasviel : AnimuHead
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
